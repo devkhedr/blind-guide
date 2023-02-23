8a561ea8beb7cba453cbdc8ab30f5605b0846bbb
Mohamed Mahmoud <107374106+MohamedKhedr07@users.noreply.github.com>
2023-02-23 23:45:04 +0200

    update api view

--- Backend/api/objectdetection/views.py
+++ Backend/api/objectdetection/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import requests
 # Create your views here.
 
 
@@ -9,16 +10,18 @@ class DetectImage(APIView):
     def post(self, request, *args, **kwargs):
         data = request.data
         print(data["image"])
+        image_data = data['image']
+        # Run the image through the YOLOv8 model and get back the results 
         return Response(
             {
-                'result': 'in progress',
-                "image": data["image"]
+                "result": "in progress",
+                "image": image_data
             }
         )
 
     def get(self, request, *args, **kwargs):
         return Response(
             {
-                'about-this-api': 'send a post request by your image, and I will respond to you by results',
+                "about-this-api": "send a post request by your image, and I will respond to you by results"
             }
         )
