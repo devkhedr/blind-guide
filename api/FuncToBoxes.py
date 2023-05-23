6c14c75be228b1177d56bc8f773887964a7f5b62
devkhedr <mohamed.khedr1714@gmail.com>
2023-05-23 19:00:38 +0300

    update api code

--- api/FuncToBoxes.py
+++ api/FuncToBoxes.py
@@ -249,6 +249,12 @@ def PlotBoxes(image, boxes, labels=[], colors=[], score=True, conf=None):
 
 
 def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+
+    Arguments:
+    box1 -- first box, list object with coordinates (box1_x1, box1_y1, box1_x2, box_1_y2)
+    box2 -- second box, list object with coordinates (box2_x1, box2_y1, box2_x2, box2_y2)
+    """
     (box1_x1, box1_y1, box1_x2, box1_y2) = box1
     (box2_x1, box2_y1, box2_x2, box2_y2) = box2
     xi1 = max(box1[0], box2[0])
--- api/app.py
+++ api/app.py
@@ -1,17 +1,27 @@
-# uvicorn app:app --port 12000 --reload
-# ngrok http 12000
+# uvicorn app:app --reload
+# ngrok http 8000
 import base64
 import cv2
-import io
+import time
+import os
 import numpy as np
 import uvicorn
 from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 from YOLOv8 import YOLOv8
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 loaded_model = YOLOv8()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ImageRequest(BaseModel):
@@ -24,7 +34,7 @@ async def read_root():
     return {"blind guide api"}
 
 
-@app.post("/detect-image/", tags=["list"])
+@app.post("/", tags=["list"])
 async def post_detect_image(image_request: ImageRequest):
     encoded_image = image_request.image_base64
     img_data = base64.b64decode(encoded_image)
@@ -34,21 +44,30 @@ async def post_detect_image(image_request: ImageRequest):
 
 @app.websocket("/detect-image/")
 async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("the connection accepted")
+    cnt = 0
     while True:
-        encoded_image = await websocket.receive_text()
-        img_data = base64.b64decode(encoded_image)
-        results = classify_image(img_data)
-        await websocket.send_json(results)
+        try:
+            encoded_image = await websocket.receive_text()
+            decoded_image = base64.b64decode(encoded_image)
+            results = classify_image(decoded_image)
+            print(results)
+            print("counting", cnt)
+            cnt += 1
+            await websocket.send_json(results)
+            print("sent")
+        except:
+            pass
+            break
 
 
 def classify_image(img_data):
     img = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_COLOR)
     conf = 0.7
-    txt, image = loaded_model.predict(img, conf)
-    retval, buffer = cv2.imencode(".jpg", image)
-    encoded_processed_image = base64.b64encode(buffer)
-    return {"list": txt, "image": encoded_processed_image}
+    txt, image, boxes = loaded_model.predict(img, conf)
+    return {"list": txt, "boxes": boxes}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=12000)
+    uvicorn.run(app, host="0.0.0.0")
